acd4a7aaafbff68000d3201f97c6ec8d11980f31
Vandana L <vandana.developer2019@gmail.com>
2019-10-05 21:39:27 +1300

    Add CamelNotation for Web Api Results

--- EmployeeMVC5/App_Start/WebApiConfig.cs
+++ EmployeeMVC5/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace EmployeeMVC5
 {
@@ -9,6 +11,10 @@ namespace EmployeeMVC5
     {
         public static void Register(HttpConfiguration config)
         {
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
