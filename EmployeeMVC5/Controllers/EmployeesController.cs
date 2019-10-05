6b9e711d24d2d87286d35391870acab23f6de951
Vandana L <vandana.developer2019@gmail.com>
2019-10-05 13:07:21 +1300

    Load Employee Details from Database

--- EmployeeMVC5/Controllers/EmployeesController.cs
+++ EmployeeMVC5/Controllers/EmployeesController.cs
@@ -9,17 +9,28 @@ namespace EmployeeMVC5.Controllers
 {
     public class EmployeesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public EmployeesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Employees
         public ActionResult Index()
         {
-            var employee = GetEmployees();
+            var employee = _context.Employees.ToList();
 
             return View(employee);
         }
 
         public ActionResult Details(int id)
         {
-            var employee = GetEmployees().SingleOrDefault(c => c.Id == id);
+            var employee = _context.Employees.SingleOrDefault(c => c.Id == id);
 
             if (employee == null)
                 return HttpNotFound();
@@ -27,15 +38,15 @@ namespace EmployeeMVC5.Controllers
             return View(employee);
         }
 
-        private IEnumerable<Employee> GetEmployees()
-        {
-            return new List<Employee>
-            {
-                new Employee { Id = 1, FirstName = "Vandana", LastName = "Caringula"},
-                new Employee { Id = 2, FirstName = "Apeksha", LastName = "Logishetty"},
-                new Employee { Id = 3, FirstName = "Apoorva", LastName = "Murali"}
+        //private IEnumerable<Employee> GetEmployees()
+        //{
+        //    return new List<Employee>
+        //    {
+        //        new Employee { Id = 1, FirstName = "Vandana", LastName = "Caringula"},
+        //        new Employee { Id = 2, FirstName = "Apeksha", LastName = "Logishetty"},
+        //        new Employee { Id = 3, FirstName = "Apoorva", LastName = "Murali"}
 
-            };
-        }
+        //    };
+        //}
     }
 }
