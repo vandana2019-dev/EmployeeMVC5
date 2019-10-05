731a04a1a71354c2fe9e203880b2c8655fe5e186
Vandana L <vandana.developer2019@gmail.com>
2019-10-05 21:08:23 +1300

    Add Api and Dtos for Employee

---
+++ EmployeeMVC5/Controllers/Api/EmployeesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeMVC5.Models;
+
+namespace EmployeeMVC5.Controllers.Api
+{
+    public class EmployeesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public EmployeesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET /api/customers
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return _context.Employees.ToList();
+        }
+
+        // GET /api/customers/1
+        public Employee GetEmployee(int id)
+        {
+            var employee = _context.Employees.SingleOrDefault(c => c.Id == id);
+
+            if (employee == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return employee;
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public Employee CreateEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+
+            return employee;
+        }
+
+        // PUT /api/employees/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Employee employee)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var employeeInDb = _context.Employees.SingleOrDefault(c => c.Id == id);
+
+            if (employeeInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            employeeInDb.FirstName = employee.FirstName;
+            employeeInDb.LastName = employee.LastName;
+            employeeInDb.IsSubscribedToNewsLetter = employee.IsSubscribedToNewsLetter;
+            employeeInDb.EmploymentTypeId = employee.EmploymentTypeId;
+
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var employeeInDb = _context.Employees.SingleOrDefault(c => c.Id == id);
+
+            if (employeeInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Employees.Remove(employeeInDb);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ EmployeeMVC5/Dtos/EmployeeDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeMVC5.Dtos
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+
+      
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+       
+        public byte EmploymentTypeId { get; set; }
+    }
+}
