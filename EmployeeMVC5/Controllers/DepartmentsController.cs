fd9914d8b32be146a368c88c27ba92913b63f782
Vandana L <vandana.developer2019@gmail.com>
2019-10-05 13:55:41 +1300

    Load Departments from DB

--- EmployeeMVC5/Controllers/DepartmentsController.cs
+++ EmployeeMVC5/Controllers/DepartmentsController.cs
@@ -9,22 +9,33 @@ namespace EmployeeMVC5.Controllers
 {
     public class DepartmentsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public DepartmentsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Departments
         public ActionResult Index()
         {
-            var departments = GetDepartments();
+            var departments = _context.Departments.ToList();
 
             return View(departments);
         }
-        private IEnumerable<Department> GetDepartments()
-        {
-            return new List<Department>
-            {
-                new Department { Id = 1, Name = "HR"},
-                new Department {Id = 1, Name = "Sales"},
-                new Department {Id = 1, Name = "IT"}
-            };
+        //private IEnumerable<Department> GetDepartments()
+        //{
+        //    return new List<Department>
+        //    {
+        //        new Department { Id = 1, Name = "HR"},
+        //        new Department {Id = 1, Name = "Sales"},
+        //        new Department {Id = 1, Name = "IT"}
+        //    };
 
-        }
+        //}
     }
 }
