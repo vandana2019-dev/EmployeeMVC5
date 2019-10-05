10ea38c38962a97156b6bd42c1aecd84f09b292d
Vandana L <vandana.developer2019@gmail.com>
2019-10-05 17:49:44 +1300

    AddNew Departments

--- EmployeeMVC5/Controllers/DepartmentsController.cs
+++ EmployeeMVC5/Controllers/DepartmentsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EmployeeMVC5.ViewModels;
 
 namespace EmployeeMVC5.Controllers
 {
@@ -27,6 +28,46 @@ namespace EmployeeMVC5.Controllers
 
             return View(departments);
         }
+
+        public ActionResult Edit(int id)
+        {
+            var department = _context.Departments.SingleOrDefault(e => e.Id == id);
+
+            if (department == null)
+                return HttpNotFound();
+
+            var viewmodel = new DepartmentFormViewModel
+            {
+                Department = department
+            };
+
+            return View("DepartmentForm", viewmodel);
+        }
+
+        public ActionResult New()
+        {
+            return View("DepartmentForm");
+        }
+
+
+        [HttpPost]
+        public ActionResult Save(Department department)
+        {
+            // Adding New Department
+            if (department.Id == 0)
+                _context.Departments.Add(department);
+
+            // Modifying an existing department
+            else
+            {
+                var departmentInDb = _context.Departments.Single(e => e.Id == department.Id);
+                departmentInDb.Name = department.Name;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Departments");
+        }
+
         //private IEnumerable<Department> GetDepartments()
         //{
         //    return new List<Department>
